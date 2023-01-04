89b72ec35437cfecde48e2dbb05767bfe58a651c
Hwasser <victorhwasser@gmail.com>
2023-01-04 17:34:32 +0100

    removed a bug that made client don't work

--- src/comService.erl
+++ src/comService.erl
@@ -3,8 +3,9 @@
 
 % TODO: Change 'mainFrame@pop-os' to something static
 init() ->
-    net_kernel:connect_node('nameServer@pop-os'),
-    {nameServer, 'nameServer@pop-os'} ! {node(), connected},
+    % Disabled for now
+    %% net_kernel:connect_node('nameServer@pop-os'),
+    %% {nameServer, 'nameServer@pop-os'} ! {node(), connected},
     DB = spawn_link(fileHandler, init, []),
     loop(DB).
 
@@ -14,6 +15,7 @@ loop(DB) ->
         % add operation coming from the UI
         {Sender, add_local, Data} ->
             DB ! {add_local, Data},
+            Sender ! {add, ok},
             loop(DB);
         % add operation coming from another client
         {Sender, add_global, Hash_tree, Add_hash} ->
